79cc0adc2d8c5bf756868b70c77d887b7d1679c0
alex02r <alex.giochi02@gmail.com>
2024-01-25 14:21:22 +0100

    [UPGRADE] Aggiunto lo style al footer

---
+++ partials/templates/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <h1>Search Hotel</h1>
+                <span>P.za degli Affari. Milano 20123 MI </span>
+            </div>
+        </div>
+    </div>
+</footer>
