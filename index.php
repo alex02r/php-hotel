65416d4677b4dd1c0c06c2c77680be0576d70390
alex02r <alex.giochi02@gmail.com>
2024-01-25 13:49:43 +0100

    [UPGRADE] Aggiunta struttura per la tabella

--- index.php
+++ index.php
@@ -15,10 +15,29 @@
         <h1>Trova il tuo hotel</h1>
     </header>
     <main>
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">Nome Hotel</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Parcheggio</th>
+                <th scope="col">Valutazione</th>
+                <th scope="col">Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+            </tbody>
+        </table>
         <?php foreach($hotels as $hotel){ ?>
             <h5> <?php echo $hotel['name']; ?> </h5>
             <p> <?php echo $hotel['description']; ?> </p>
-            <span><?php echo $hotel['parking'] ?? 'Parcheggio disponibile' ; ?></span>
+            <span><?php echo $hotel['parking'] ?? 'Parcheggio disponibile'; ?></span>
             <span>Voto: <?php echo $hotel['vote']; ?></span>
             <span>Distanza dal centro: <?php echo $hotel['distance_to_center']; ?></span>
             <hr>
